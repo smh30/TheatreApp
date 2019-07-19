cf043cac7918195cda2fc6031eea910087880ca2
Stephanie Hope <steph.hope@gmail.com>
2019-07-19 20:04:30 +1200

    Initial commit of blank project

---
+++ src/main/java/theatre/audition/AuditionApplication.java
@@ -0,0 +1,13 @@
+package theatre.audition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditionApplication.class, args);
+    }
+
+}
